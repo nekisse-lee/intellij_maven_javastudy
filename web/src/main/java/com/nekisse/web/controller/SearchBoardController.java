26d99f251f50d6bfba95a14e60fffdb78a083923
Nekisse-window <lsh891224@gmail.com>
2018-12-12 23:12:05 +0900

    window spring -  web  댓글 수정, 삭제처리

--- web/src/main/java/com/nekisse/web/controller/SearchBoardController.java
+++ web/src/main/java/com/nekisse/web/controller/SearchBoardController.java
@@ -1,5 +1,7 @@
 package com.nekisse.web.controller;
 
+import com.nekisse.web.domain.BoardVO;
+import com.nekisse.web.domain.Criteria;
 import com.nekisse.web.domain.PageMaker;
 import com.nekisse.web.domain.SearchCriteria;
 import com.nekisse.web.service.BoardService;
@@ -11,6 +13,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("sboard")
@@ -34,6 +40,64 @@ public class SearchBoardController {
 
         model.addAttribute("pageMaker", pageMaker);
         return "sboard/list";
+    }
+
+    @RequestMapping(value = "readPage", method = RequestMethod.GET)
+    public String read(
+            @RequestParam("bno") Integer bno, @ModelAttribute("cri") SearchCriteria cri, Model model) throws Exception {
+        model.addAttribute(service.read(bno));
+        return "sboard/readPage";
+
+    }
+
+    @RequestMapping(value = "removePage", method = RequestMethod.POST)
+    public String read(@RequestParam Map map, @ModelAttribute("cri") SearchCriteria cri, RedirectAttributes rttr) throws Exception {
+        service.remove(map);
+
+        rttr.addFlashAttribute("page", cri.getPage());
+        rttr.addFlashAttribute("perPageNum", cri.getPerPageNum());
+        rttr.addFlashAttribute("searchType", cri.getSearchType());
+        rttr.addFlashAttribute("keyword", cri.getKeyword());
+
+        rttr.addFlashAttribute("mag", "SUCCESS");
+        return  "redirect:/sboard/listPage";
+    }
+
+    @RequestMapping(value = "modifyPage", method = RequestMethod.GET)
+    public String modifyPage(
+            @RequestParam("bno") Integer bno, @ModelAttribute("cri") SearchCriteria cri, Model model) throws Exception {
+        model.addAttribute("boardVO", service.read(bno));
+        return "sboard/modifyPage";
+    }
+
+    @RequestMapping(value = "modifyPage", method = RequestMethod.POST)
+    public String modifyPagePOST(@RequestParam Map map, @ModelAttribute("cri") SearchCriteria cri, RedirectAttributes rttr) throws Exception {
+
+        service.modify(map);
+
+        rttr.addFlashAttribute("page", cri.getPage());
+        rttr.addFlashAttribute("perPageNum", cri.getPerPageNum());
+        rttr.addFlashAttribute("searchType", cri.getSearchType());
+        rttr.addFlashAttribute("keyword", cri.getKeyword());
+
+        rttr.addFlashAttribute("msg", "SUCCESS");
+        return "redirect:/sboard/list";
+    }
+
+    @RequestMapping(value = "register", method = RequestMethod.GET)
+    public String  registerGET(BoardVO board, Model model)throws Exception {
+        logger.info("register get.......");
+        return "sboard/register";
+    }
+
+    @RequestMapping(value = "register", method = RequestMethod.POST)
+    public String registerPOST(BoardVO board, Model model, RedirectAttributes rttr) throws Exception {
+        logger.info("register POST......");
+        logger.info(board.toString());
+
+        service.regist(board);
+        rttr.addFlashAttribute("msg", "SUCCESS");
+        return "redirect:/sboard/list";
     }
 
 
