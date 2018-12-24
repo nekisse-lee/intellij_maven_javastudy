fc2ff9f49f0c7fb80e1478b136709313a18398d2
nekisse <lsh891224@gmail.com>
2018-12-24 18:25:58 +0900

    mac java 스프링 project(chap08) DB 연동

--- sp5chap007/src/main/java/aspect/CacheAspect.java
+++ sp5chap007/src/main/java/aspect/CacheAspect.java
@@ -3,7 +3,6 @@ package aspect;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
 
 import java.util.HashMap;
 import java.util.Map;
