4a6cc45de9a8062cefe413725e2219b43c2f2e77
nekisse <lsh891224@gmail.com>
2018-12-26 17:30:27 +0900

    mac java 스프링 project(chap08) DB 연동

--- sp5chap08/src/main/java/config/AppCtx.java
+++ sp5chap08/src/main/java/config/AppCtx.java
@@ -3,6 +3,7 @@ package config;
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import spring.MemberDao;
 
 @Configuration
 public class AppCtx {
@@ -13,11 +14,17 @@ public class AppCtx {
         DataSource ds = new DataSource();
         ds.setDriverClassName("com.mysql.jdbc.Driver");
         ds.setUrl("jdbc:mysql://localhost/spring5fs?characterEncoding=utf8");
-        ds.setUsername("spring5fs");
-        ds.setPassword("spring5fs");
+        ds.setUsername("spring5");
+        ds.setPassword("spring5");
         ds.setInitialSize(2);
         ds.setMaxActive(10);
         return ds;
+    }
+
+
+    @Bean
+    public MemberDao memberDao() {
+        return new MemberDao(dataSource());
     }
 
 
