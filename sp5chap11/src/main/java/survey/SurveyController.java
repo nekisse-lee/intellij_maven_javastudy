27b72739dcef55498a52d047f70692248ca84ace
nekisse <lsh891224@gmail.com>
2019-01-01 15:58:37 +0900

    mac java 스프링 project(chap11) MVC                @Valid

--- sp5chap11/src/main/java/survey/SurveyController.java
+++ sp5chap11/src/main/java/survey/SurveyController.java
@@ -1,14 +1,14 @@
 package survey;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 @Controller
