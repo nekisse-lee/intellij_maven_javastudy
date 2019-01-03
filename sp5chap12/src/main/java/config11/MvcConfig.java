c867ac47cfd01e6eba7e1199b3ed75fcdd1e0dec
nekisse <lsh891224@gmail.com>
2019-01-03 17:28:12 +0900

    mac java 스프링 project(chap13) MVC                세션, 인터셉터, 쿠키

--- sp5chap12/src/main/java/config11/MvcConfig.java
+++ sp5chap12/src/main/java/config11/MvcConfig.java
@@ -1,11 +1,9 @@
 package config11;
 
-import controller11.RegisterRequestValidator;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.validation.Validator;
 import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
