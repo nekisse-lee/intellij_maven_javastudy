e10404582e442bbfbf3f9f27a115292eb591a7ec
nekisse <lsh891224@gmail.com>
2019-01-06 23:31:09 +0900

    mac java 스프링 project(chap14) MVC                  LocalDateTime 변환

---
+++ sp5chap14/src/main/java/controller14/MemberDetailController.java
@@ -0,0 +1,30 @@
+package controller14;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import spring14.Member;
+import spring14.MemberDao;
+import spring14.MemberNotFoundException;
+
+@Controller
+public class MemberDetailController {
+
+  private MemberDao memberDao;
+
+  public void setMemberDao(MemberDao memberDao) {
+    this.memberDao = memberDao;
+  }
+
+  @GetMapping("/members/{id}")
+  public String detail(@PathVariable("id") Long memId, Model model) {
+    Member member = memberDao.selectById(memId);
+    if (member == null) {
+      throw new MemberNotFoundException();
+    }
+    model.addAttribute("member", member);
+    return "member/memberDetail";
+  }
+
+}
