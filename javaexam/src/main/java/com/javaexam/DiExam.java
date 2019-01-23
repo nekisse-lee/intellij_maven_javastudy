f6f40f5bd3dcbb11cadaf7d1484ecc2bd930fbc9
nekisse <lsh891224@gmail.com>
2019-01-23 23:03:41 +0900

    mac java  Exam

---
+++ javaexam/src/main/java/com/javaexam/DiExam.java
@@ -0,0 +1,35 @@
+package com.javaexam;
+
+class Member {
+  private  int age;
+  private String name;
+
+  public Member() {
+  }
+
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
+
+
+public class DiExam {
+  public static void main(String[] args) {
+
+    Member member = new Member();
+
+  }
+}
---
+++ javaexam/src/main/java/com/javaexam/StringExam.java
@@ -0,0 +1,25 @@
+package com.javaexam;
+
+public class StringExam {
+
+  public static void main(String[] args) {
+
+    String str = "hello" + "world";
+    System.out.println("str = " + str);
+
+    System.out.println(str.replace("hello", "HELLO"));
+    System.out.println("str = " + str);
+
+
+    StringBuilder stringBuilder = new StringBuilder();
+    stringBuilder.append("hello");
+    stringBuilder.append("world");
+    System.out.println("stringBuilder = " + stringBuilder);
+    System.out.println("stringBuilder = " + stringBuilder.replace(0,5,"HELLO"));
+    System.out.println("stringBuilder = " + stringBuilder);
+
+  }
+
+
+
+}
