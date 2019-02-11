e6ec0c5de0247f33e441f029256171198d4e6072
nekisse <lsh891224@gmail.com>
2019-02-11 16:17:27 +0900

    TDD 연습 prime factors

---
+++ javaexam/src/test/java/primefactors/PrimeFactorsTest.java
@@ -0,0 +1,45 @@
+package primefactors;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class PrimeFactorsTest {
+  @Test
+  public void canFactorIntoPrimes() {
+    assertPrimeFactors(1, list());
+    assertPrimeFactors(2, list(2));
+    assertPrimeFactors(3, list(3));
+    assertPrimeFactors(4, list(2, 2));
+    assertPrimeFactors(5, list(5));
+    assertPrimeFactors(6, list(2, 3));
+    assertPrimeFactors(7, list(7));
+    assertPrimeFactors(8, list(2, 2, 2));
+    assertPrimeFactors(9, list(3, 3));
+    assertPrimeFactors(2 * 2 * 2 * 3 * 3 * 5 * 7 * 11 * 13, list(2 , 2 , 2 , 3 , 3 , 5 , 7 , 11 , 13));
+
+
+  }
+
+  private void assertPrimeFactors(int n, List<Integer> list) {
+    assertEquals(list, of(n));
+  }
+
+  private List<Integer> list(Integer... ints) {
+    return Arrays.asList(ints);
+  }
+
+  private List<Integer> of(int n) {
+    ArrayList<Integer> factors = new ArrayList<>();
+    int divisor = 2;
+    for (; n > 1; divisor++)
+      for (; n % divisor == 0; n /= divisor)
+        factors.add(divisor);
+    return factors;
+  }
+
+}
