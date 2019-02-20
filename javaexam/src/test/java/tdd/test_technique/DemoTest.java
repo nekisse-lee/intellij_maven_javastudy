f23dcdf323b38c02c027022cd3c904500a84283b
nekisse <lsh891224@gmail.com>
2019-02-20 14:28:45 +0900

    mac java  spring boot-           bootstrap

--- javaexam/src/test/java/tdd/test_technique/DemoTest.java
+++ javaexam/src/test/java/tdd/test_technique/DemoTest.java
@@ -10,6 +10,51 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class DemoTest {
+
+    @Test
+    public void sort() {
+        assertThat(sort(intList()), is(intList()));
+        assertThat(sort(intList(1)), is(intList(1)));
+        assertThat(sort(intList(2, 1)), is(intList(1, 2)));
+        assertThat(sort(intList(2, 3, 1)), is(intList(1, 2, 3)));
+        assertThat(sort(intList(1, 3, 2)), is(intList(1, 2, 3)));
+        assertThat(sort(intList(3, 1, 2)), is(intList(1, 2, 3)));
+        assertThat(sort(intList(3, 2, 1, 2)), is(intList(1, 2, 2, 3)));
+    }
+
+    private List<Integer> sort(List<Integer> list) {
+        ArrayList<Integer> sorted = new ArrayList<>();
+        if (list.size() == 0)
+            return list;
+        /*else if (list.size() == 2) {
+            if (list.get(0) > list.get(1)) {
+                sorted.add(list.get(1));
+                sorted.add(list.get(0));
+            } else {
+                sorted.add(list.get(0));
+                sorted.add(list.get(1));
+            }
+        }*/
+        else {
+            List<Integer> low = new ArrayList<>();
+            Integer mid = list.get(0);
+            List<Integer> high = new ArrayList<>();
+
+            for (Integer i : list.subList(1,list.size())) {
+                if (i < mid)
+                    low.add(i);
+                else
+                    high.add(i);
+            }
+            if (low != null)
+                sorted.addAll(sort(low));
+            sorted.add(mid);
+            if (high != null)
+                sorted.addAll(sort(high));
+        }
+        return sorted;
+    }
+
     @Test
     public void factors() {
         assertThat(factorsOf(1), is(intList()));
@@ -21,7 +66,6 @@ public class DemoTest {
         assertThat(factorsOf(7), is(intList(7)));
         assertThat(factorsOf(8), is(intList(2, 2, 2)));
         assertThat(factorsOf(9), is(intList(3, 3)));
-
     }
 
     private List<Integer> intList(Integer... i) {
