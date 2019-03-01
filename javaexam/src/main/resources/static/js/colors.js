c7243c8237cff37d06f2d6778bfed6e10c4a374c
nekisse <lsh891224@gmail.com>
2019-03-01 18:15:20 +0900

    mac java  javascript

---
+++ javaexam/src/main/resources/static/js/colors.js
@@ -0,0 +1,45 @@
+let Links = {
+  setColor: function(color) {
+    let aList = document.querySelectorAll('a');
+    let i = 0;
+    while (i < aList.length) {
+      aList[i].style.color = color;
+      i = i + 1;
+    }
+  }
+};
+
+
+
+
+let Body = {
+  setColor: function(color) {
+    document.querySelector('body').style.color = color;
+  },
+  setBackgroundColor : function(color) {
+    document.querySelector('body').style.backgroundColor = color;
+  }
+};
+
+
+
+
+
+function nightDayHandler(self) {
+  if (self.value === 'night') {
+    Body.setBackgroundColor('black');
+    Body.setColor('white');
+    self.value = 'day';
+
+    Links.setColor('yellow');
+
+
+  } else {
+    Body.setBackgroundColor('white');
+    Body.setColor('black');
+    self.value = 'night';
+
+    Links.setColor('blue')
+  }
+}
+
