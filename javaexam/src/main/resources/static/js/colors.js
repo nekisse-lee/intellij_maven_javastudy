cf3ddeb76816ca7151f890403847d0eb66016a5b
nekisse <lsh891224@gmail.com>
2019-03-02 20:32:00 +0900

    mac java  javascript

--- javaexam/src/main/resources/static/js/colors.js
+++ javaexam/src/main/resources/static/js/colors.js
@@ -1,11 +1,13 @@
 let Links = {
   setColor: function(color) {
-    let aList = document.querySelectorAll('a');
-    let i = 0;
-    while (i < aList.length) {
-      aList[i].style.color = color;
-      i = i + 1;
-    }
+    // let aList = document.querySelectorAll('a');
+    // let i = 0;
+    // while (i < aList.length) {
+    //   aList[i].style.color = color;
+    //   i = i + 1;
+    // }
+
+    $('a').css('color', color);
   }
 };
 
@@ -14,10 +16,12 @@ let Links = {
 
 let Body = {
   setColor: function(color) {
-    document.querySelector('body').style.color = color;
+    // document.querySelector('body').style.color = color;
+    $('body').css('color', color);
   },
   setBackgroundColor : function(color) {
-    document.querySelector('body').style.backgroundColor = color;
+    // document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor', color);
   }
 };
 
